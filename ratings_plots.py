a59401034d525a2d1e300480948d454780f27855
Jeremy Magland <jmagland@flatironinstitute.org>
2025-04-07 11:29:50 -0400

    ratings

--- ratings_plots.py
+++ ratings_plots.py
@@ -22,7 +22,7 @@ df['model'] = df['model'].apply(lambda x: ' '.join(x))
 questions = [q['name'] for q in questions_data['questions']]
 
 # Set style and colors
-colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728']  # Default matplotlib colors
+colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 plt.rcParams['figure.facecolor'] = 'white'
 plt.rcParams['axes.facecolor'] = 'white'
 
