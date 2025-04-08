f34b29f0e790983c9f88e9db74da24e5979a77f9
Jeremy Magland <jmagland@flatironinstitute.org>
2025-04-08 12:09:16 -0400

    submodules

--- dandisets/001333/2025-04-04-gemini-2.0-flash-001/working/tmp_scripts/list_assets.py
+++
@@ -1,9 +0,0 @@
-from dandi.dandiapi import DandiAPIClient
-
-client = DandiAPIClient()
-dandiset = client.get_dandiset("001333")
-assets = list(dandiset.get_assets())
-
-# Print the paths of the first 5 assets
-for i, asset in enumerate(assets[:5]):
-    print(f"Asset {i+1}: {asset.path}")
